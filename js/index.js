3ecb02a424255a5fc46b719287db14126c89ccf7
TheIllusiveC4 <gasingdong@gmail.com>
2019-06-18 13:33:42 -0700

    add three more event listeners

--- js/index.js
+++ js/index.js
@@ -1,9 +1,23 @@
 // Your code goes here
-const btns = document.querySelectorAll('.btn');
+function toggleClass(event, name) {
+  event.target.classList.toggle(name);
+}
 
-btns.forEach(btn =>
-  btn.addEventListener(
-    'click',
-    event => (event.target.style.backgroundColor = 'red')
-  )
-);
+const nav = document.querySelectorAll('nav a');
+nav.forEach(link => {
+  link.addEventListener('click', event => toggleClass(event, 'red-bg'));
+});
+
+const imgs = document.querySelectorAll('img');
+
+imgs.forEach(img => {
+  img.addEventListener('mouseenter', event => {
+    toggleClass(event, 'black-border');
+  });
+  img.addEventListener('mouseleave', event => {
+    toggleClass(event, 'black-border');
+  });
+});
+
+const body = document.querySelector('body');
+body.addEventListener('keydown', event => toggleClass(event, 'blue-bg'));
