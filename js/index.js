5793825877b93bc6a9f9351f24ae0265b19d5a97
TheIllusiveC4 <gasingdong@gmail.com>
2019-06-18 12:08:08 -0700

    add first event listener

--- js/index.js
+++ js/index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+const btns = document.querySelectorAll('.btn');
+
+btns.forEach(btn =>
+  btn.addEventListener(
+    'click',
+    event => (event.target.style.backgroundColor = 'red')
+  )
+);
